ee4b4ecaa63ba963ba0484957a7fa7411251101d
Pascal Vogel <pascal-vogel@gmx.de>
2023-08-16 19:16:09 +0200

    Pyramide kann nun mit mehreren Zeichen erstellt werden

--- Program.cs
+++ Program.cs
@@ -5,13 +5,22 @@ int höhe = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Welches Zeichen soll als Inhalt gewählt werden?");
 string s = Console.ReadLine();
 
+int länge = s.Length;
+
+string leerzeichen = "";
+
+for (int i = 0; i < länge; i++)
+{
+    leerzeichen += " ";
+}
+
 string s1 = s + s;
 
 for (int i = höhe; i > 0; i--)
 {
     for (int j = i; j > 1; j--)
     {
-        Console.Write(" ");
+        Console.Write(leerzeichen);
     }
 
     Console.WriteLine(s);
