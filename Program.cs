a8cc838de4301e035b8af19f65afb008af6a368b
Pascal Vogel <pascal-vogel@gmx.de>
2023-08-16 19:08:33 +0200

    Pyramide kann erstellt werden

--- Program.cs
+++ Program.cs
@@ -2,6 +2,18 @@
 
 int höhe = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(höhe);
+string s = "x";
+string s1 = s + s;
+
+for (int i = höhe; i > 0; i--)
+{
+    for (int j = i; j > 1; j--)
+    {
+        Console.Write(" ");
+    }
+
+    Console.WriteLine(s);
+    s += s1;
+}
 
 Console.ReadKey();
